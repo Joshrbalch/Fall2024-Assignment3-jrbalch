e91f61072494b4bef4da264dd6829ab6b7441371
Josh Balch <Joshrbalch@gmail.com>
2024-10-10 21:34:27 -0500

    initial commit

---
+++ Fall2024-Assignment3-jrbalch/Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+
+namespace Fall2024_Assignment3_jrbalch.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string? Title { get; set; }
+        public string? Genre { get; set; }
+        public string? IMDBLink { get; set; }
+        public int? releaseYear { get; set; }
+        public BitArray? Poster { get; set; }
+    }
+}
