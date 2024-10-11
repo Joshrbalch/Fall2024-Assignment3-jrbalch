3f023d4b9ce0bacfae63d1416eb53467d697f64f
Josh Balch <64498806+Joshrbalch@users.noreply.github.com>
2024-10-10 22:42:23 -0500

    Delete Fall2024-Assignment3-jrbalch directory

--- Fall2024-Assignment3-jrbalch/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Fall2024_Assignment3_jrbalch.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
